26b9cffb87b0fe552bde91a8f33d9447ec52f7ce
Vishnu Jayadevan <vishnu@deepsource.io>
2024-05-08 19:13:17 +0530

    initial commit

---
+++ pages/blog.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>image</div>
+    <div class="flex items-center justify-center w-full">
+        <div class="w-1/2">
+            <h1 class="text-8xl text-center">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            </h1>
+            <div class="mt-8 text-center">
+                <div>
+                    <span class="text-2xl">By&nbsp;</span>
+                    <span class="text-2xl font-bold">John Doe</span>
+                    <span class="text-2xl">&nbsp;on&nbsp;</span>
+                    <span class="text-2xl font-bold">2021-01-01</span>
+                </div>
+                
+            </div>
+            <p class="text-2xl font-serif mt-16">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non autem, enim vel iusto voluptatum debitis maxime quisquam exercitationem nam nobis tempora unde ipsam? Quae ab ipsam assumenda labore id itaque.   Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo perferendis consectetur maiores quasi placeat inventore tenetur tempora eius rerum ipsam? Totam illum, atque ea nostrum corporis voluptatum nam voluptatem amet.
+            </p>
+        </div>
+    </div>
+</template>
