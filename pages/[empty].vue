6897d72498cc89f16c7a9dc33edadb66975fdd99
Vishnu Jayadevan <blueyellowpencil@gmail.com>
2024-09-18 10:26:40 +0530

    build out typography and complete nav

---
+++ pages/[empty].vue
@@ -0,0 +1,12 @@
+<template>
+    <NuxtLayout name="home">
+        <div class="flex flex-col gap-4 items-center justify-center h-screen font-garamond">
+            <h1 class="text-9xl">404</h1>
+            <nuxt-link to="/" class="font-garamond">~ take me home</nuxt-link>
+        </div>
+    </NuxtLayout>
+    
+</template>
+
+<script setup lang="ts">
+</script>
---
+++ pages/blog/category/[category].vue
@@ -0,0 +1,26 @@
+
+<template>
+    <NuxtLayout>
+        <template #header>
+            <PageTitle :title="category" :breadcrumbs="breadcrumbs()"/>
+        </template> 
+        <template #body>
+            <Toc :filter="category" :showCategory="false"/>
+        </template>
+    </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+
+import type { BreadCrumb } from '~/components/PageTitle.vue';
+
+const breadcrumbs = (): BreadCrumb[] => {
+    const breadcrumbs: BreadCrumb[] = [
+    { label: '~', to: '/' },
+    { label: 'Blog', to: '/blog' },
+    ];
+    return breadcrumbs;
+};
+const route = useRoute();
+const category = Array.isArray(route.params.category) ? route.params.category.join(',') : route.params.category;
+</script>
