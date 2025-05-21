219c7e0b11118b06cb4455ed73c6047e0b10c351
Vishnu Jayadevan <blueyellowpencil@gmail.com>
2025-05-21 14:54:42 +0530

    feat: update home page style and nav bar

---
+++ components/about/LanguageSwitcher.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="font-sans text-xs tracking-wider mb-2">
+    <nuxt-link
+      class="cursor-pointer p-1 hover:bg-black hover:text-white"
+      active-class="bg-black text-white"
+      to="/about"
+      >EN</nuxt-link
+    >
+    <span class="mx-2">|</span>
+    <nuxt-link
+      class="cursor-pointer p-1 hover:bg-black hover:text-white"
+      active-class="bg-black text-white"
+      to="/about/malayalam"
+      >ML</nuxt-link
+    >
+  </div>
+</template>
