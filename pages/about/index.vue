0ae56142fb15f78d175b467b1e231b39ec39b5ce
Vishnu Jayadevan <blueyellowpencil@gmail.com>
2025-05-22 12:33:07 +0530

    fix about content

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -8,28 +8,36 @@
       <div class="pb-8">
         <div class="mb-8">
           <div class="text-xl font-light font-garamond leading-relaxed">
-            I am <span class="font-bold">Vishnu Jayadevan</span>, a software
-            engineer based in Bangalore, India. I am currently at
-            <a link href="https://deepsource.com" class="font-bold">DeepSource</a>, building tools to help developers
-            write better code. This is my
-            little corner of the internet where I write about distributed
-            systems, software architecture and leadership in tech. You'll
-            occasionally find posts in other topics like biochemistry,
-            aquascaping and travel. You might also find some posts in Malayalam
-            for the folks back home.
+            I'm <span class="font-bold">Vishnu Jayadevan</span>, a software
+            engineer based in Bangalore, India. I work at
+            <a link href="https://deepsource.com" class="font-bold"
+              >DeepSource</a
+            >, where I work on the latest generation of code quality and
+            security tools. This site is my little corner of the internet, where
+            I share thoughts on distributed systems, software architecture, and
+            leadership in tech. From time to time, I also write about other
+            interests—like biochemistry, aquascaping, and travel. You might even
+            come across the occasional post in Malayalam, for folks back home.
           </div>
         </div>
         <h2 class="text-4xl mb-4">Career</h2>
-
         <div class="mb-8 pl-8 grid lg:grid-cols-2 gap-y-4">
           <div class="flex items-center">
             <div>
               <h3 class="">DeepSource</h3>
-              <div>(2024-Present)</div>
+              <div>(2020-Present)</div>
             </div>
             <span class="flex-grow hidden lg:block">
-              <svg viewBox="0 0 100 10" xmlns="http://www.w3.org/2000/svg" class="w-full h-2">
-                <path d="M0,5 C10,0 20,10 30,5 C40,0 50,10 60,5 C70,0 80,10 90,5" stroke="black" fill="transparent" />
+              <svg
+                viewBox="0 0 100 10"
+                xmlns="http://www.w3.org/2000/svg"
+                class="w-full h-2"
+              >
+                <path
+                  d="M0,5 C10,0 20,10 30,5 C40,0 50,10 60,5 C70,0 80,10 90,5"
+                  stroke="black"
+                  fill="transparent"
+                />
               </svg>
             </span>
           </div>
@@ -69,29 +77,30 @@
               <div>(2015-2019)</div>
             </div>
             <span class="flex-grow hidden lg:block">
-              <svg viewBox="0 0 100 10" xmlns="http://www.w3.org/2000/svg" class="w-full h-2">
-                <path d="M0,5 C10,0 20,10 30,5 C40,0 50,10 60,5 C70,0 80,10 90,5" stroke="black" fill="transparent" />
+              <svg
+                viewBox="0 0 100 10"
+                xmlns="http://www.w3.org/2000/svg"
+                class="w-full h-2"
+              >
+                <path
+                  d="M0,5 C10,0 20,10 30,5 C40,0 50,10 60,5 C70,0 80,10 90,5"
+                  stroke="black"
+                  fill="transparent"
+                />
               </svg>
             </span>
           </div>
           <div>
-            <h4 class="text-lg font-semibold">
-              Senior Software Engineer - Product Engineering
-            </h4>
-            <p>Building the core API platform and developer tools at Exotel.</p>
-            <hr class="my-2" />
-            <h4 class="text-lg font-semibold">
-              Software Development Engineer | Enterprise Technology
-            </h4>
+            <h4 class="text-lg font-semibold">Senior Software Engineer</h4>
             <p>
-              Building state-of-the-art cloud telephony solutions for large
-              enterprises and launched a new product line.
+              This was my first job after college. At 21, I joined a small team
+              of engineers and had the privilege of working with some of the
+              best minds in the industry. I wore many hats, ranging from
+              building internal tools to developing a new telephony platform for
+              the enterprise market that powered billions of phone
+              conversations. This work resulted in my first granted patent for
+              innovations in telephony infrastructure.
             </p>
-            <hr class="my-2" />
-            <h4 class="text-lg font-semibold">
-              Software Development Engineer | Product Engineering
-            </h4>
-            <p>Building internal developer experience tools.</p>
           </div>
         </div>
       </div>
